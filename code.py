b4b6723dab29d0a95eba802f7d5450f9a5402320
Patrick <patrickdesousabr@gmail.com>
2025-12-02 20:44:31 -0300

    Criação da função de cadastro

--- code.py
+++ code.py
@@ -1,16 +1,20 @@
-from utility.funcoes import menu, indice
+from utility.funcoes import menu, indice, cadastrar
 
-menu()
-chave_menu = indice()
+elenco = []
 
-if chave_menu == 1:
-    print()
-elif chave_menu == 2:
-    print()
-elif chave_menu == 3:
-    print()
-elif chave_menu == 4:
-    print()
-else:
-    print("-="*32)
-    print('Dígite um índice válido.')
+while True:
+    menu()
+    chave_menu = indice()
+
+    if chave_menu == 1:
+        jogador = cadastrar()
+        elenco.append(jogador)
+    elif chave_menu == 2:
+        print(elenco)
+    elif chave_menu == 3:
+        print()
+    elif chave_menu == 4:
+        break
+    else:
+        print("-="*32)
+        print('Dígite um índice válido.')
