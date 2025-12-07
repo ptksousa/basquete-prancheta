ca0f5572d2084dfc891dd2e1ff8215992e7e19d5
Patrick <patrickdesousabr@gmail.com>
2025-12-07 16:37:07 -0300

    Criação da visualização da lista dos jogadores cadastrados

--- code.py
+++ code.py
@@ -1,4 +1,7 @@
-from utility.funcoes import menu, indice, cadastrar, salvar_json, carregar_json
+import time
+
+from utility.funcoes import menu, indice, cadastrar
+from utility.json_funcoes import salvar_json, carregar_json
 
 elenco = carregar_json()
 
@@ -10,13 +13,27 @@ while True:
             jogador = cadastrar()
             elenco.append(jogador)
             salvar_json(elenco)
+            time.sleep(0.5)
             print('Jogador cadastrado com sucesso.')
+            time.sleep(1)
+
     elif chave_menu == 2:
-        print(elenco)
+        print("-="*32)
+        print("LISTA DE JOGADORES".center(64))
+        print("-="*32)
+        for i, jogador in enumerate(elenco):
+             print(f'[{i}] NOME: {jogador['NOME']}')
+             print(f'    IDADE: {jogador['IDADE']}')
+             print(f'    POSICAO: {jogador['POSICAO']}')
+             print("-"*64)
+        input('Pressione ENTER para retornar.')
+
     elif chave_menu == 3:
         print('EM BREVE')
+
     elif chave_menu == 4:
         break
+
     else:
         print("-="*32)
         print('Dígite um índice válido.')
--- utility/funcoes.py
+++ utility/funcoes.py
@@ -1,7 +1,3 @@
-import json
-
-ARQUIVO = "elenco.json"
-
 def menu():
     print("-="*32)
     print("INÍCIO".center(64))
@@ -26,16 +22,20 @@ def cadastrar():
     print("-="*32)
     print("CADASTRO DE JOGADOR".center(64))
     print("-="*32)
+
     nome = input('Digíte o primeiro nome do jogador: ').upper().strip()
     sobrenome = input('Digíte o último nome do jogador: ').upper().strip()
     nome_completo = nome + ' ' + sobrenome
+
     while True:
         try:
             idade = int(input('Digíte a idade do jogador: '))
             break
         except ValueError:
             print('!!!Digíte uma idade válida!!!')
+
     posicoes = ['ARMADOR', 'ALA-ARMADOR', 'ALA', 'ALA-PIVÔ', 'PIVÔ']
+
     for i, posicao in enumerate(posicoes):
         print(f'[{i}] {posicao}')
               
@@ -47,18 +47,5 @@ def cadastrar():
         "IDADE" : idade,
         "POSICAO" : posicao
         } 
-    return jogador
-
-
-def carregar_json():
-    try:
-        with open(ARQUIVO, "r", encoding="utf-8") as open_file:
-            return json.load(open_file)
-    except:
-        return []
-
-
-def salvar_json(dados):
-    with open(ARQUIVO, "w", encoding="utf-8") as write_file:
-        json.dump(dados, write_file, indent=4, ensure_ascii=False)
     
+    return jogador
